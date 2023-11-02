afe8a8f9b8f4bdf8597bd92f3d95ca4fb8233796
djakish <69299717+djakish@users.noreply.github.com>
2023-11-02 07:13:20 +0600

    bundler

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -3,9 +3,11 @@ import lin_font_r from '../assets/fonts/LinLibertine_R.ttf'
 import lin_font_rb from '../assets/fonts/LinLibertine_RB.ttf'
 import lin_font_rbi from '../assets/fonts/LinLibertine_RBI.ttf'
 
-import init, { addFont, setSource, renderSvgMerged } from '@djakish/render-typst'
+import  init, {addFont, setSource, renderSvgMerged } from '@djakish/render-typst'
 
-await init();
+addFont(lin_font_r)
+addFont(lin_font_rb)
+addFont(lin_font_rbi)
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -16,10 +18,6 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <div id="preview"></div>
   </div>
 `
-
-await addFont(lin_font_r)
-await addFont(lin_font_rb)
-await addFont(lin_font_rbi)
 
 const button = document.querySelector<HTMLButtonElement>('#renderer')!;
 
